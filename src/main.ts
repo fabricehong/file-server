3cca65d587d957b58741832922325437c81a6ae8
Fabrice Hong <hong.fabrice@moviplus.ch>
2020-08-24 17:26:39 +0200

    fix(BDORAPP-1426): improve logging

--- src/main.ts
+++ src/main.ts
@@ -8,11 +8,10 @@ async function bootstrap() {
   const logger: Logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
   const port = configuration.getServerPort();
-  const host = 'http://localhost';
   app.enableCors();
   app.useGlobalFilters(new GlobalHttpErrorHandler());
   await app.listen(port);
   logger.log(`Serving files in: ${configuration.getStorageRoot()}`);
-  logger.log(`listening on port ${host}:${port}`);
+  logger.log(`listening on port http://localhost:${port}`);
 }
 bootstrap();
