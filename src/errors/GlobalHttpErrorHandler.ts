3273b7170df7384b39f62a029dab497a4a93135b
Fabrice Hong <hong.fabrice@moviplus.ch>
2020-08-24 21:49:01 +0200

    feat(BDORAPP-1426): logging errors

--- src/errors/GlobalHttpErrorHandler.ts
+++ src/errors/GlobalHttpErrorHandler.ts
@@ -1,11 +1,24 @@
 import { ArgumentsHost, Catch, ExceptionFilter, HttpException, Logger, } from '@nestjs/common';
+import { Request, Response } from 'express';
 
-@Catch()
+@Catch(HttpException)
 export class GlobalHttpErrorHandler implements ExceptionFilter {
     private readonly logger: Logger = new Logger(GlobalHttpErrorHandler.name);
 
     public catch(exception: HttpException, host: ArgumentsHost): any {
-        this.logger.error(exception);
-        throw exception;
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status = exception.getStatus();
+
+        this.logger.error(`${request.url}: ${exception.message}`);
+
+        response
+            .status(status)
+            .json({
+                statusCode: status,
+                message: exception.message,
+                path: request.url,
+            });
     }
 }
